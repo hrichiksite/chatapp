a6dc77f4eedd26a9d48dbd27e4d99dc003432313
hrichiksite <57860916+hrichiksite@users.noreply.github.com>
2020-05-15 12:12:22 +0200

    Update auth.js

--- src/static/js/auth.js
+++ src/static/js/auth.js
@@ -1,7 +1,7 @@
 var OKTA_SETTINGS = {
-  baseUrl: "https://dev-111464.oktapreview.com",
-  clientId: "0oaejf8gmll1TiDRz0h7",
-  issuer: "https://dev-111464.oktapreview.com/oauth2/default"
+  baseUrl: "https://dev-400984.okta.com",
+  clientId: "0oac5zsqbEDZ7cUpj4x6",
+  issuer: "https://dev-400984.okta.com/oauth2/default"
 };
 
 var okta = new OktaSignIn({
